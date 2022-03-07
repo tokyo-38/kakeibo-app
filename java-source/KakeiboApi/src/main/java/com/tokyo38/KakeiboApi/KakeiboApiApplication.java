4611983d8b5b6aaca70bd167beecee53f591e6ce
tokyo-38k <tokyo-38k@gmail.com>
2022-03-07 22:14:20 +0900

    java example code

---
+++ java-source/KakeiboApi/src/main/java/com/tokyo38/KakeiboApi/KakeiboApiApplication.java
@@ -0,0 +1,13 @@
+package com.tokyo38.KakeiboApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboApiApplication.class, args);
+	}
+
+}
---
+++ java-source/KakeiboApi/src/test/java/com/tokyo38/KakeiboApi/KakeiboApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokyo38.KakeiboApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeiboApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
